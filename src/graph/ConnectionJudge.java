9a2faa60fae94cb4479d5164377e723ec29f9baf
kunihiko-harada <sthsoulful@gmail.com>
2013-05-26 22:52:43 +0900

    algorithm project for practice

---
+++ src/graph/ConnectionJudge.java
@@ -0,0 +1,32 @@
+package graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ConnectionJudge {
+
+	int[][] edge;
+
+	boolean isConnect(int from, int to, int v) {
+		boolean[] dp = new boolean[v];
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(from);
+		dp[from] = true;
+
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			for (int i = 0; i < edge[now].length; i++) {
+				int next = edge[now][i];
+				if (dp[next]) {
+					continue;
+				}
+				if (next == to) {
+					return true;
+				}
+				dp[next] = true;
+				q.add(next);
+			}
+		}
+		return false;
+	}
+}
